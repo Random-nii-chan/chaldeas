c31b97f0d5a256ac53cb08e0c23de33d4419431d
randomniichan <leokolo@hotmail.fr>
2020-03-19 11:13:49 +0100

    premier fragment

--- app/src/main/java/fr/spc/leosoliveres/chaldeas/model/Station.kt
+++ app/src/main/java/fr/spc/leosoliveres/chaldeas/model/Station.kt
@@ -2,6 +2,6 @@ package fr.spc.leosoliveres.chaldeas.model
 
 import fr.spc.leosoliveres.chaldeas.model.Site
 
-class Station(_nom:String, _latitude:Double, _longitude:Double, _energie:String, _acces:String,
-              _proprietaire:String, _type:String):
+class Station(_nom:String, _latitude:Double, _longitude:Double, _energie:String="Non renseigné", _acces:String="Non renseigné",
+              _proprietaire:String="Non renseigné", _type:String="Non renseigné"):
     Site(_nom,_longitude,_latitude,_energie,_acces, _proprietaire, _type)
