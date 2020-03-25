c6ddb76195dc6060382848018f6c8ec4f4168413
randomniichan <leokolo@hotmail.fr>
2020-03-25 11:47:20 +0100

    custom toolbar

--- app/src/main/java/fr/spc/leosoliveres/chaldeas/ui/MainActivity.kt
+++ app/src/main/java/fr/spc/leosoliveres/chaldeas/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package fr.spc.leosoliveres.chaldeas.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import fr.spc.leosoliveres.chaldeas.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,14 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		setSupportActionBar(findViewById(R.id.toolbar))
+		supportActionBar?.setDisplayShowHomeEnabled(true)
+		supportActionBar?.setLogo(R.mipmap.ic_launcher)
+		supportActionBar?.setDisplayUseLogoEnabled(true)
+	}
 
+	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+		menuInflater.inflate(R.menu.toolbar,menu)
+		return super.onCreateOptionsMenu(menu)
 	}
 }
