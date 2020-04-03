73fa6349c634d0f189a2e2387fcc34a4263ad223
randomniichan <leokolo@hotmail.fr>
2020-04-03 12:07:13 +0200

    recyclerview report edit fragment

---
+++ app/src/main/java/fr/spc/leosoliveres/chaldeas/adapter/MeasuresAdapter.kt
@@ -0,0 +1,4 @@
+package fr.spc.leosoliveres.chaldeas.adapter
+
+class MeasuresAdapter {
+}
--- app/src/main/java/fr/spc/leosoliveres/chaldeas/ui/fragment/ReportEditFragment.kt
+++ app/src/main/java/fr/spc/leosoliveres/chaldeas/ui/fragment/ReportEditFragment.kt
@@ -1,18 +1,31 @@
 package fr.spc.leosoliveres.chaldeas.ui.fragment
 
+import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+
 import fr.spc.leosoliveres.chaldeas.R
 
-class ReportEditFragment:Fragment() {
-	override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_site_detail, container, false)
-	}
+class ReportEditFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ReportEditFragment()
+    }
+
+    private lateinit var viewModel: ReportEditViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.report_edit_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ReportEditViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
 }
